4a34fec2bb0781f8ee7c7d9327e87fd491a24a3a
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2024-06-22 22:21:16 +0500

    added environment parsing in config

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var Params struct {
@@ -12,4 +13,16 @@ var Params struct {
 func init() {
 	flag.StringVar(&Params.ServerAddress, "a", "localhost:8080", "Server address like localhost:8080")
 	flag.StringVar(&Params.ShortUrlPrefix, "b", "localhost:8080", "Prefix of short url like (localhost:8080)/url_code")
+
+	for _, v := range []struct {
+		from string
+		to   *string
+	}{
+		{"SERVER_ADDRESS", &Params.ServerAddress},
+		{"BASE_URL", &Params.ShortUrlPrefix},
+	} {
+		if envValue := os.Getenv(v.from); envValue != "" {
+			*v.to = envValue
+		}
+	}
 }
